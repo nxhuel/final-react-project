fd63f358bbbe5b7dfc535435effec6a11117aa58
nxhuel <nahueltisera03@gmail.com>
2023-11-27 14:46:34 -0300

    Finalizando componente products

--- proyectoFinal/src/App.jsx
+++ proyectoFinal/src/App.jsx
@@ -1,17 +1,25 @@
 import Product from './components/Product';
 import mantenimiento from './assets/mantenimiento.jpg';
+import zapatillaUno from './assets/zapatillaUno.png';
+import zapatillaDos from './assets/zapatillaDos.png';
+import zapatillaTres from './assets/zapatillaTres.png';
+import zapatillaCuatro from './assets/zapatillaCuatro.png';
+import zapatillaCinco from './assets/zapatillaCinco.png';
+import zapatillaSeis from './assets/zapatillaSeis.png';
+import zapatillaSiete from './assets/zapatillaSiete.png';
+import zapatillaOcho from './assets/zapatillaOcho.png';
 import './App.css';
 
 function App() {
   const productsData = [
-    { id: 1, nombre: 'Producto 1', precio: 19.99, imagen: mantenimiento },
-    { id: 2, nombre: 'Producto 2', precio: 29.99, imagen: mantenimiento },
-    { id: 3, nombre: 'Producto 3', precio: 39.99, imagen: mantenimiento },
-    { id: 4, nombre: 'Producto 4', precio: 49.99, imagen: mantenimiento },
-    { id: 5, nombre: 'Producto 5', precio: 59.99, imagen: mantenimiento },
-    { id: 6, nombre: 'Producto 6', precio: 69.99, imagen: mantenimiento },
-    { id: 7, nombre: 'Producto 7', precio: 79.99, imagen: mantenimiento },
-    { id: 8, nombre: 'Producto 8', precio: 89.99, imagen: mantenimiento },
+    { id: 1, nombre: 'Producto 1', precio: 19.99, imagen: zapatillaUno },
+    { id: 2, nombre: 'Producto 2', precio: 29.99, imagen: zapatillaDos },
+    { id: 3, nombre: 'Producto 3', precio: 39.99, imagen: zapatillaTres },
+    { id: 4, nombre: 'Producto 4', precio: 49.99, imagen: zapatillaCuatro },
+    { id: 5, nombre: 'Producto 5', precio: 59.99, imagen: zapatillaCinco},
+    { id: 6, nombre: 'Producto 6', precio: 69.99, imagen: zapatillaSeis },
+    { id: 7, nombre: 'Producto 7', precio: 79.99, imagen: zapatillaSiete },
+    { id: 8, nombre: 'Producto 8', precio: 89.99, imagen: zapatillaOcho },
   ];
 
   return (
